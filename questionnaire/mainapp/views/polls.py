56496091e6f084be63eebf9a1ff18fb986a9db84
Asman <iasman312@gmail.com>
2021-04-03 12:28:01 +0600

    Создали шаблон для пагинации и шаблон для вывода всех вопросов

--- questionnaire/mainapp/views/polls.py
+++ questionnaire/mainapp/views/polls.py
@@ -5,7 +5,7 @@ from mainapp.models import Poll
 class IndexView(ListView):
     template_name = 'polls/index.html'
     model = Poll
-    context_object_name = 'articles'
+    context_object_name = 'polls'
     ordering = '-created_at'
     paginate_by = 5
     paginate_orphans = 1
