046c6f4c0da13a92ae0a60a38a16d32fcdb098e1
Asman <iasman312@gmail.com>
2021-04-03 11:47:21 +0600

    Создали модели для Опроса и варианта и сделали миграцию

--- questionnaire/mainapp/models.py
+++ questionnaire/mainapp/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=1000, null=False, blank=False,
+                                verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Choice(models.Model):
+    choice = models.TextField(max_length=1000, null=False, blank=False,
+                              verbose_name='Вариант ответа')
+    poll = models.ForeignKey('mainapp.Poll', on_delete=models.CASCADE,
+                             related_name='choices', verbose_name='Опрос',
+                             null=False, blank=False)
+
+
