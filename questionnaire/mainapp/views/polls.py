163276b7fccf4696672f6bfeb77a5a05a116ae17
Asman <iasman312@gmail.com>
2021-04-03 12:17:35 +0600

    Создали вьюшку для вывода всех опросов

---
+++ questionnaire/mainapp/views/polls.py
@@ -0,0 +1,11 @@
+from django.views.generic import ListView
+from mainapp.models import Poll
+
+
+class IndexView(ListView):
+    template_name = 'polls/index.html'
+    model = Poll
+    context_object_name = 'articles'
+    ordering = '-created_at'
+    paginate_by = 5
+    paginate_orphans = 1
